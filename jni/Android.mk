20b1246e771f6a1230053eb53f339136a045e116
Aaron N. Rudolph <aanrudolph2@gmail.com>
2015-05-27 10:50:29 -0400

    Added shader-specific source to makefile

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := gallery
-LOCAL_SRC_FILES := gallery.c
+LOCAL_SRC_FILES := util/shader.c gallery.c
 LOCAL_LDLIBS 	:= -lGLESv2
 
 include $(BUILD_SHARED_LIBRARY)
