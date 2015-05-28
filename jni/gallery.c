b5830abeb5d80a9aaadf149a313e5ed14af225e3
Aaron N. Rudolph <aanrudolph2@gmail.com>
2015-05-28 10:57:03 -0400

    Added vertex/fragment shader read, compile, and add to GL program.

--- jni/gallery.c
+++ jni/gallery.c
@@ -1,12 +1,49 @@
 #include "gallery.h"
-#include <GLES2/gl2.h>
-#include <stdio.h>
 
 extern const char * fShaderPath;
 extern const char * vShaderPath;
 
 JNIEXPORT void JNICALL Java_com_rudy_artgallery_GallerySurfaceView_createGLSurface(JNIEnv * env, jclass cls)
 {
+	long int frag_shader_len = -1;
+	long int vert_shader_len = -1;
+
+	char * frag_shader_src;
+	char * vert_shader_src;
+
+	FILE * frag_shader = fopen(fShaderPath, "rb");
+	FILE * vert_shader = fopen(vShaderPath, "rb");
+
+	// Get length of fragment shader
+	if(fseek(frag_shader, 0, SEEK_END))
+	{
+		frag_shader_len = ftell(frag_shader);
+		frag_shader_src = malloc(frag_shader_len * sizeof(char));
+
+		frag_shader_len = fread(frag_shader_src, 1, frag_shader_len, frag_shader);
+	}
+
+	fclose(frag_shader);
+
+	// Get length of vertex shader
+	if(fseek(vert_shader, 0, SEEK_END))
+	{
+		vert_shader_len = ftell(vert_shader);
+		vert_shader_src = malloc(vert_shader_len * sizeof(char));
+
+		vert_shader_len = fread(vert_shader_src, 1, vert_shader_len, vert_shader);
+	}
+
+	fclose(vert_shader);
+
+	if(frag_shader_len > -1 && vert_shader_len > -1)
+	{
+		build_program(vert_shader_src, vert_shader_len, frag_shader_src, frag_shader_len);
+
+		free(vert_shader_src);
+		free(frag_shader_src);
+	}
+
 	glClearColor(1, 0, 0, 0);
 }
 JNIEXPORT void JNICALL Java_com_rudy_artgallery_GallerySurfaceView_changeGLSurface(JNIEnv * env, jclass cls, jint w, jint h)
